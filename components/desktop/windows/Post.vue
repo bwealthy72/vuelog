300ae807a78bc130f11616d93d87f69045888da0
bwealthy72 <bwealthy72@gmail.com>
2022-07-05 10:03:11 +0900

    속도가 fetch 문제인건지 확인

--- components/desktop/windows/Post.vue
+++ components/desktop/windows/Post.vue
@@ -76,19 +76,18 @@ export default {
   },
 
   async fetch() {
-    let category = this.$route.params.category;
-    if (!category) category = "";
-
-    await this.$store.dispatch("notion/getCategories");
-    await this.$store.dispatch("notion/getPosts", category);
-    if (this.$route.params.id) {
-      await this.$store.dispatch("notion/getPost", this.$route.params.id);
-    } else {
-      await this.$store.dispatch(
-        "notion/getPost",
-        this.$store.state.notion.posts[0].id
-      );
-    }
+    // let category = this.$route.params.category;
+    // if (!category) category = "";
+    // await this.$store.dispatch("notion/getCategories");
+    // await this.$store.dispatch("notion/getPosts", category);
+    // if (this.$route.params.id) {
+    //   await this.$store.dispatch("notion/getPost", this.$route.params.id);
+    // } else {
+    //   await this.$store.dispatch(
+    //     "notion/getPost",
+    //     this.$store.state.notion.posts[0].id
+    //   );
+    // }
   },
 };
 </script>
