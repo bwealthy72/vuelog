78b3a2e7207c8a3f4f8113cd7ec1512eec8df015
bwealthy72 <bwealthy72@gmail.com>
2022-07-05 13:46:37 +0900

    cache control 제거

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,3 @@
     <MobileAppList v-else />
   </div>
 </template>
-
-<script>
-export default {
-  middleware: cacheControl({
-    "max-age": 60,
-    "stale-when-revalidate": 5,
-  }),
-};
-</script>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,12 +1,3 @@
 <template>
   <DesktopWindowList />
 </template>
-
-<script>
-export default {
-  middleware: cacheControl({
-    "max-age": 60,
-    "stale-when-revalidate": 5,
-  }),
-};
-</script>
