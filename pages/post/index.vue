a3796eb8cc16aaa22bf7531a986d8ab47b472868
bwealthy72 <bwealthy72@gmail.com>
2022-07-05 13:35:09 +0900

    cacheControl 추가

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,3 +1,12 @@
 <template>
   <DesktopWindowList />
 </template>
+
+<script>
+export default {
+  middleware: cacheControl({
+    "max-age": 60,
+    "stale-when-revalidate": 5,
+  }),
+};
+</script>
